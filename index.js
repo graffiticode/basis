ef0302f84d90ddf47970363c434ad1350611442a
Jeff Dyer <jeffdyer@acm.org>
2021-04-06 13:54:00 -0700

    Add BasisRender

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import * as basis from './src/basis-compiler.js'
 export const BasisChecker = basis.BasisChecker;
 export const BasisTransformer = basis.BasisTransformer;
+export const BasisRenderer = basis.BasisRenderer;
