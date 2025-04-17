43be5f42f6da09c5254ac2727a1e500980a1e983
Jeff Dyer <jeffdyer@acm.org>
2025-04-17 11:21:40 -0700

    Add missing file

---
+++ src/lexicon.js
@@ -0,0 +1,191 @@
+export const lexicon = {
+  "print" : {
+    "tk": 1,
+    "name": "PRINT",
+    "cls": "function",
+    "length": 1,
+    "arity": 1
+  },
+  "get" : {
+    "tk": 1,
+    "name": "GET",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "set" : {
+    "tk": 1,
+    "name": "SET",
+    "cls": "function",
+    "length": 3,
+    "arity": 3
+  },
+  "nth" : {
+    "tk": 1,
+    "name": "NTH",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "sub" : {
+    "tk": 1,
+    "name": "SUB",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "filter" : {
+    "tk": 1,
+    "name": "FILTER",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "reduce" : {
+    "tk": 1,
+    "name": "REDUCE",
+    "cls": "function",
+    "length": 3,
+    "arity": 3
+  },
+  "map" : {
+    "tk": 1,
+    "name": "MAP",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "lt" : {
+    "tk": 1,
+    "name": "LT",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "le" : {
+    "tk": 1,
+    "name": "LE",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "gt" : {
+    "tk": 1,
+    "name": "GT",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "ge" : {
+    "tk": 1,
+    "name": "GE",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "ne" : {
+    "tk": 1,
+    "name": "NE",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "len" : {
+    "tk": 1,
+    "name": "LEN",
+    "cls": "function",
+    "length": 1,
+    "arity": 1
+  },
+  "concat" : {
+    "tk": 1,
+    "name": "CONCAT",
+    "cls": "function",
+    "length": 1,
+    "arity": 1
+  },
+  "add" : {
+    "tk": 1,
+    "name": "ADD",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "mul" : {
+    "tk": 1,
+    "name": "MUL",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "pow" : {
+    "tk": 1,
+    "name": "POW",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "map" : {
+    "tk": 1,
+    "name": "MAP",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "apply" : {
+    "tk": 1,
+    "name": "APPLY",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "data" : {
+    "tk": 1,
+    "name": "DATA",
+    "cls": "function",
+    "length": 1,
+    "arity": 1
+  },
+  "json" : {
+    "tk": 1,
+    "name": "JSON",
+    "cls": "function",
+    "length": 1,
+    "arity": 1
+  },
+  "eq" : {
+    "tk": 1,
+    "name": "EQ",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "mod" : {
+    "tk": 1,
+    "name": "MOD",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "min" : {
+    "tk": 1,
+    "name": "MIN",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "max" : {
+    "tk": 1,
+    "name": "MAX",
+    "cls": "function",
+    "length": 2,
+    "arity": 2
+  },
+  "range" : {
+    "tk": 1,
+    "name": "RANGE",
+    "cls": "function",
+    "length": 3,
+    "arity": 3
+  }
+}
